50b65113c52b32e8b8aaa36b5a24fc14a258cdbd
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-03-21 20:32:54 -0500

    Pregunta 6
    Pregunta 6 - lab 1

--- pregunta_06.sql
+++ pregunta_06.sql
@@ -36,10 +36,11 @@
 --    K0  K1     c12  c13         c14   c15   c16
 --  0  A  20  938.16  300  2016-09-12  0.19  BECB
 --  1  A  30  135.80  900  2017-01-26  0.23  EGAB
---  2  A  18  142.99  100  2017-02-12  0.48  GGFD
+--  2  A  18  142.99  100  2017-02-12  0.48  GGFDPregunta 5
 --  3  A  26  456.47  400  2018-01-28  0.11  FGED
 --  4  A   6  391.42  300  2018-05-15  0.22  BFGB
 --  5  A  10  816.51  600  2019-04-25  0.40  DAGC
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
+SELECT * FROM tbl1 WHERE K0 = 'A' ORDER BY c14
